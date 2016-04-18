f53c90ac5c16cdd851d5e7509872cff71a19327b
JoshuaMitton <joshmitton2@gmail.com>
2016-04-18 13:33:21 +0100

    Update myConstants.h
    comment

--- myConstants.h
+++ myConstants.h
@@ -1,5 +1,5 @@
 #ifndef MYCONSTANTS_H
 #define MYCONSTANTS_H
 const int DataSize = 100000;
-const int BufferSize = 8192;
+const int BufferSize = 8192; // set up buffer with a size
 #endif // MYCONSTANTS_H
