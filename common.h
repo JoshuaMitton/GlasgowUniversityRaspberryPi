4d736fc2c56b1261a271494d7d7f8a0c86a88c00
JoshuaMitton <joshmitton2@gmail.com>
2016-04-18 13:21:03 +0100

    Update common.h
    Added in comments

--- common.h
+++ common.h
@@ -4,14 +4,14 @@
 #include <QSemaphore>
 #include "myConstants.h"
 
-extern int buffer[BufferSize];
+extern int buffer[BufferSize]; // Ring buffer
 
-extern QSemaphore freeBytes;
-extern QSemaphore usedBytes;
+extern QSemaphore freeBytes; // Check for free bytes in the ring buffer
+extern QSemaphore usedBytes; // Check for used bytes in the ring buffer
 
-extern int Josh;
-extern int plotvalue;
-extern int milk;
-extern int user_temperature;
+extern int Josh; // Used as a global variable where it is required in numerous threads
+extern int plotvalue; // Global vatiable for plotting
+extern int milk; // Global variable for amount of milk specified
+extern int user_temperature; // Global variable for user selected temperature
 
 #endif // COMMON_H
