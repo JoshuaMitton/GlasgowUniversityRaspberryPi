5f587ecac2e64d321ea3321d8053eb3154a3c05c
JoshuaMitton <joshmitton2@gmail.com>
2016-04-18 13:32:48 +0100

    Update main.cpp
    comments

--- main.cpp
+++ main.cpp
@@ -6,10 +6,10 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    ConProdDialog w;
+    ConProdDialog w; // Create an instance where the whole program will run from
 
     w.showMaximized();
-    w.startTimer(1);
+    w.startTimer(1); // Start the application
 
     return a.exec();
 }
